c6f3bed58aa1a05855c28792e8d8e82b0e56fb8f
ApurbaHasanJ <apurbahasanj@gmail.com>
2024-05-17 03:35:51 +0600

    project init 2

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
 
 // console.log(process.cwd()) //D:\Apurba\ph-mongoose-ts
